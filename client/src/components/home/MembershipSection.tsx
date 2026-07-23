e584b6d80fd04229827d4dbeb5f5f8b3ae880597
Arindam Dasgupta <arindamdasgupta6@gmail.com>
2026-07-23 12:32:50 +0530

    Updated membership section in homepage

--- client/src/components/home/MembershipSection.tsx
+++ client/src/components/home/MembershipSection.tsx
@@ -5,7 +5,7 @@ export default function MembershipSection() {
   return (
     <section className="py-24 xl:py-32 bg-white px-6 md:px-10">
       <div className="max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-12 gap-12 items-center">
-        <div className="lg:col-span-7 h-[450px] overflow-hidden">
+        <div className="lg:col-span-7 h-112.5 overflow-hidden">
           <img src={assets.membership_section_img} alt="Chef's Table" className="w-full h-full object-cover transition-transform duration-500 hover:scale-105" />
         </div>
 
