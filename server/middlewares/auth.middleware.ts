4ebdad296dda637660aa611900edeb6594e0edfb
Arindam Dasgupta <arindamdasgupta6@gmail.com>
2026-07-24 18:40:29 +0530

    Add authentication middleware to protect routes and verify user tokens

---
+++ server/middlewares/auth.middleware.ts
@@ -0,0 +1,44 @@
+import { NextFunction, Request, Response } from "express";
+import { IUser, UserModel } from "../models/user.model.js";
+import jwt from "jsonwebtoken";
+
+export interface AuthRequest extends Request {
+  user?: IUser;
+}
+
+// Middleware to protect routes and ensure the user is authenticated
+export const protect = async (
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction,
+): Promise<void> => {
+  let token: string | undefined;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET!) as {
+        id: string;
+      };
+      const user = await UserModel.findById(decoded.id).select("-password");
+
+      if (!user) {
+        res.status(401).json({ message: "User not found" });
+        return;
+      }
+
+      req.user = user;
+      next();
+    } catch (error) {
+      console.error(error);
+      res.status(401).json({ message: "Not authorized, token failed" });
+      return;
+    }
+  } else {
+    res.status(401).json({ message: "Not authorized, no token" });
+    return;
+  }
+};
