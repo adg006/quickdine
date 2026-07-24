6a736689504e032ca7c805d635b1a2077eb65fd8
Arindam Dasgupta <arindamdasgupta6@gmail.com>
2026-07-24 19:24:40 +0530

    Add comment to clarify MongoDB connection function

--- server/config/db.ts
+++ server/config/db.ts
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 
+// Connect to MongoDB
 const connectDB = async () => {
   mongoose.connection.on("connected", () => console.log("DB Connected"));
   await mongoose.connect(process.env.MONGODB_URI!);
